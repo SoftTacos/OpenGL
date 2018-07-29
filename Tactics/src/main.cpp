320e0d10d6d9e30897aadcff9748ee071aec349c
SoftTacos <onlykindanerdy@gmail.com>
2018-07-29 13:55:48 -0600

    GLBuffer and glVertexAttribPointer

--- Tactics/src/main.cpp
+++ Tactics/src/main.cpp
@@ -1,6 +1,7 @@
 #pragma once
 //#define GLEW_STATIC
 
+#include <time.h>
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
@@ -32,32 +33,34 @@ int main(void)
 	fprintf(stdout, "Status: Using GLEW %s\n", glewGetString(GLEW_VERSION));
 	std::cout << glGetString(GL_VERSION) << std::endl;
 
-	float positions[] = { -0.5f, -0.5f,
+	float positions[] = { 
+		-0.5f, -0.5f,
 		0.5f, -0.5f,
-		0.5f, 0.0f
+		0.0f, 0.5f
 	};
 
 	unsigned int buffer1;
 	glGenBuffers(1, &buffer1);
 	glBindBuffer(GL_ARRAY_BUFFER, buffer1);
+	//http://docs.gl/gl4/glVertexAttribPointer
+	//stride is number of bytes between the vertices, going to have to know how many bytes are in each vertex
+	
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, (const void*)0);
+	
 	//specify how big the buffer is
 	glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
 
     /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window))
-    {
+    while (!glfwWindowShouldClose(window)){
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
 		glDrawArrays(GL_TRIANGLES, 0, 3);
+		//glDrawElements(GL_TRIANGLES, 3, ) //leaving this hook to work with and learn later
+		//vertex in openGL can contain way more than just position data, position, texture coords, normals, etc.
+		
 
-		/*
-		glBegin(GL_TRIANGLES);
-		glVertex2f(-0.5f, -0.5f);
-		glVertex2f(0.5f, -0.5f);
-		glVertex2f(0.5f, 0.0f);
-		glEnd();
-		*/
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
 
